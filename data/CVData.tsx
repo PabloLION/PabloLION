4dc5141f146b8be7b2145cbaacbf2e7353147467
Pablion <36828324+Pablion@users.noreply.github.com>
2022-02-06 03:00:44 +0100

    feat: add CVData

--- data/CVData.tsx
+++ data/CVData.tsx
@@ -45,5 +45,69 @@ export const CVData = {
       time: "2020 OCT - 2021 JUN (DROPPED)",
       content: "Master in Applied Mathematics",
     },
+  ],
+  "experience": [
+    {
+      name: "Name",
+      category: "Category",
+      comment: "Comment",
+    },
+    {
+      name: "Personal Blog",
+      category: "Blog",
+      comment: "A Hexo based blog",
+    },
+    {
+      name: "Snippet Hub",
+      category: "VSCode extension",
+      comment: "Working in progress Project to Find, Share and Rate Snippets",
+    },
+    {
+      name: "Chrome Terminal",
+      category: "Chrome extension",
+      comment: "React-based productivity tool, imitating a Terminal-like interface",
+    },
+  ],
+  "skill": [
+    {
+      name: "Name",
+      level: "Level",
+    },
+    {
+      name: "React - web",
+      level: "advanced level",
+    },
+    {
+      name: "ES6, Typescript",
+      level: "advanced",
+    },
+    {
+      name: "Markdown",
+      level: "Git Flavor",
+    },
+    {
+      name: "GitHub & Git",
+      level: "intermediate level",
+    },
+    {
+      name: "Name",
+      level: "Level",
+    },
+    {
+      name: "Matlab",
+      level: "advanced level",
+    },
+    {
+      name: "Python",
+      level: "advanced level",
+    },
+    {
+      name: "LaTeX",
+      level: "intermediate level",
+    },
+    {
+      name: "Vue.js",
+      level: "intermediate level",
+    },
   ]
 }
