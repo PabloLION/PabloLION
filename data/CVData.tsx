75998d1377f72a1398a5a5a1633de0afab6856ac
Pablion <36828324+Pablion@users.noreply.github.com>
2022-02-06 02:52:13 +0100

    feat: add CVData

---
+++ data/CVData.tsx
@@ -0,0 +1,49 @@
+export const CVData = {
+  "name": {
+    surname: [
+      { name: "施", pinyin: "shī" },
+    ],
+    given: [
+      { name: "云", pinyin: "yún" },
+      { name: "瀚", pinyin: "hàn" }
+    ]
+  },
+  "education": [
+    {
+      institution: "Institution",
+      city: "City",
+      time: "Time",
+      content: "Content",
+    },
+    {
+      institution: "Xi'an Jiaotong University",
+      city: "Xi'an, Shaanxi, China",
+      time: "2010 SEP - 2017 JUNE",
+      content: "Bachelor of Science in Informational and Computational Mathematics",
+    },
+    {
+      institution: "Palencia International Junior College",
+      city: "Palencia, Spain",
+      time: "2017 OCT - 2018 JULY",
+      content: "Spanish Course",
+    },
+    {
+      institution: "Universitat Politècnica de Catalunya",
+      city: "Barcelona, Spain",
+      time: "2018 OCT - 2019 JAN (DROPPED)",
+      content: "Master's degree in Advanced Mathematics and Mathematical Engineering",
+    },
+    {
+      institution: "Escola Oficial d'Idiomes Drassanes",
+      city: "Barcelona, Spain",
+      time: "2019 FEB - 2020 SEP",
+      content: "Spanish and English Language Course",
+    },
+    {
+      institution: "Universitat de Barcelona",
+      city: "Barcelona, Spain",
+      time: "2020 OCT - 2021 JUN (DROPPED)",
+      content: "Master in Applied Mathematics",
+    },
+  ]
+}
